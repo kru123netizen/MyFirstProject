56b51eaa595574f400a406f14ace4d552390ea17
Black_Coffe12 <kurniarahman987@gmail.com>
2025-02-08 12:10:43 +0700

    menambahkan variabel nilai

--- First/first_proyek.py
+++ First/first_proyek.py
@@ -1,1 +1,16 @@
+first_name = "kurnia"
+mid_name = "_"
+last_name = "rahman"
+
+print(first_name + mid_name + last_name)
+
+
+A = 57 + (22 * 19) / 3 - 12
+print(A)
+
+B = 27
+print (A - B)
+
+
+
 print("Hello World")
